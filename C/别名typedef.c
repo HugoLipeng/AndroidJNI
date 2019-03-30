a3a33a7ffa7a0dccb3739c6ab8b5835d4fc590ab
HugoLipeng <1924949262@qq.com>
2019-03-30 17:47:05 +0800

    别名typedef

---
+++ C/别名typedef.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef int i;
+typedef long l;
+typedef float f;
+int main()
+{
+    i m = 10;
+    l n = 123123123;
+    f s = 3.1415;
+    printf("%d\n", m);
+    printf("%ld\n", n);
+    printf("%f\n", s);
+    return 0;
+}
