2efac470591b9181c83071d6eaef49e037cb1770
Hugo <lipeng@qiniu.com>
2019-12-09 12:12:31 +0800

    FFmpeg解封装

--- FFmpegStepbyStep/app/src/main/cpp/native-lib.cpp
+++ FFmpegStepbyStep/app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,14 @@
 
 extern "C"{
 #include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+}
+#include<iostream>
+using namespace std;
+
+static double r2d(AVRational r)
+{
+    return r.num==0||r.den == 0 ? 0 :(double)r.num/(double)r.den;
 }
 
 extern "C" JNIEXPORT jstring JNICALL
@@ -14,6 +22,88 @@ Java_com_hugo_ffmpegstepbystep_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     hello += avcodec_configuration();
+    //初始化解封装
+    av_register_all();
+    //初始化网络
+    avformat_network_init();
+
+    //打开文件
+    AVFormatContext *ic = NULL;
+    char path[] = "/sdcard/1080.mp4";
+    //char path[] = "/sdcard/video.flv";
+    int re = avformat_open_input(&ic,path,0,0);
+    if(re != 0)
+    {
+        LOGW("avformat_open_input failed!:%s",av_err2str(re));
+        return env->NewStringUTF(hello.c_str());
+    }
+    LOGW("avformat_open_input %s success!",path);
+    //获取流信息
+    re = avformat_find_stream_info(ic,0);
+    if(re != 0)
+    {
+        LOGW("avformat_find_stream_info failed!");
+    }
+    LOGW("duration = %lld nb_streams = %d",ic->duration,ic->nb_streams);
+
+    int fps = 0;
+    int videoStream = 0;
+    int audioStream = 1;
+
+    for(int i = 0; i < ic->nb_streams; i++)
+    {
+        AVStream *as = ic->streams[i];
+        if(as->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+        {
+            LOGW("视频数据");
+            videoStream = i;
+            fps = r2d(as->avg_frame_rate);
+
+            LOGW("fps = %d,width=%d height=%d codeid=%d pixformat=%d",fps,
+                 as->codecpar->width,
+                 as->codecpar->height,
+                 as->codecpar->codec_id,
+                 as->codecpar->format
+            );
+        }
+        else if(as->codecpar->codec_type ==AVMEDIA_TYPE_AUDIO )
+        {
+            LOGW("音频数据");
+            audioStream = i;
+            LOGW("sample_rate=%d channels=%d sample_format=%d",
+                 as->codecpar->sample_rate,
+                 as->codecpar->channels,
+                 as->codecpar->format
+            );
+        }
+    }
+    //ic->streams[videoStream];
+    //获取音频流信息
+    audioStream = av_find_best_stream(ic,AVMEDIA_TYPE_AUDIO,-1,-1,NULL,0);
+    LOGW("av_find_best_stream audioStream = %d",audioStream);
+
+    //读取帧数据
+    AVPacket *pkt = av_packet_alloc();
+    for(;;)
+    {
+        int re = av_read_frame(ic,pkt);
+        if(re != 0)
+        {
+
+            LOGW("读取到结尾处!");
+            int pos = 20 * r2d(ic->streams[videoStream]->time_base);
+            av_seek_frame(ic,videoStream,pos,AVSEEK_FLAG_BACKWARD|AVSEEK_FLAG_FRAME );
+            continue;
+        }
+        LOGW("stream = %d size =%d pts=%lld flag=%d",
+             pkt->stream_index,pkt->size,pkt->pts,pkt->flags
+        );
+        //////////////////////
+
+        av_packet_unref(pkt);
+    }
+    // 关闭上下文
+    avformat_close_input(&ic);
     return env->NewStringUTF(hello.c_str());
 }
 extern "C"
